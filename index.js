469532ef39d7a13c2018183bfd9eb3f2b0615d77
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-09-22 14:28:56 +0100

    two tests written for subtract, code refactored to pass both

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const sum = (numA, numB) => {
   return typeof numA !== 'number' || typeof numB !== 'number' ? 'Enter a valid number' : numA + numB
 }
 
+const subtract = (numA, numB) => {
+  return typeof numA !== 'number' || typeof numB !== 'number' ? 'Enter a valid number' : numA - numB
+}
+
 exports.sum = sum
+exports.subtract = subtract
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 
-const { sum } = require('./index')
+const { sum, subtract } = require('./index')
 
 test('adds two integers together', () => {
   expect(sum(1,2)).toBe(3)
@@ -11,4 +11,12 @@ test('does not add strings together', () => {
 
 test('does not add arrays', () => {
   expect(sum([1,2,3],[4,5,6])).toBe('Enter a valid number')
+})
+
+test('subtracts second argument from first', () => {
+  expect(subtract(2,1)).toBe(1)
+})
+
+test('does not subtract non-numerical data types', () => {
+  expect(subtract('foo', 'oo')).toBe('Enter a valid number')
 })
